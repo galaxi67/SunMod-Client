2eee738f8ae133879f9d19a980a0f6022e27abdc
waysxc <wayslos92@gmail.com>
2024-12-20 18:52:07 +0700

    perbaikan responsive halaman metode

--- src/pages/Metode.jsx
+++ src/pages/Metode.jsx
@@ -38,7 +38,7 @@ const ImageGrid = () => {
 export const Metode = () => {
   return (
     <div className="container mx-auto">
-      <div className="py-10 bg-gradient-to-l from-blue-50 via-blue-400 to-custom-blue mt-3 sm:mt-5 lg:mt-15 rounded-custom-br">
+      <div className="py-10 bg-gradient-to-l from-blue-50 via-blue-400 to-custom-blue mt-3 sm:mt-5 lg:mt-15 rounded-custom-br mx-5 lg:mx-1">
         <div className="max-w-full mx-auto px-4 sm:px-8 md:px-10 lg:px-20 grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="title-content space-y-4 my-auto text-white">
             <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold tracking-widest">METODE SUMOD</h1>
