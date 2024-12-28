4608d684f077ccc2f831ee32b46845973fd64e28
ways92 <d.ferdiansya@gmail.com>
2024-12-29 00:42:40 +0700

    fix api

--- src/admin/api.js
+++ src/admin/api.js
@@ -5,7 +5,7 @@ const BASE_URL = "http://localhost:5000/api";
 export const fetchData = async (tableName) => {
   try {
     const response = await axios.get(`${BASE_URL}/${tableName}`);
-    return response.data;
+    return response.data.data;
   } catch (error) {
     console.error(`Error fetching data from ${tableName}:`, error.response || error.message);
     throw error;
@@ -17,7 +17,7 @@ export const updateData = async (tableName, id, formData) => {
     const response = await axios.put(`${BASE_URL}/${tableName}/${id}`, formData, {
       headers: { "Content-Type": "multipart/form-data" },
     });
-    return response.data;
+    return response.data.data;
   } catch (error) {
     console.error(`Error updating data in ${tableName}:`, error.response || error.message);
     throw error;
