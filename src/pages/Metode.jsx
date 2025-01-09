bed3bf3786081709c6d4067c4e4efcc582e70c76
ways92 <d.ferdiansya@gmail.com>
2025-01-10 05:11:28 +0700

    fix metode

--- src/pages/Metode.jsx
+++ src/pages/Metode.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { fetchData } from "../admin/api/apiService";
-import LoadingIndicator from "../components/LoadingIndicator"
+import LoadingIndicator from "../components/LoadingIndicator";
 
-const ImageCard = ({ src, alt }) => (
-  <div className="p-4 w-[130px] h-[130px] sm:w-[140px] sm:h-[140px] md:w-[150px] md:h-[150px] lg:w-[200px] lg:h-[200px] bg-blue-50 rounded-custom-br">
+const ImageCard = ({ src, alt, onClick }) => (
+  <div
+    className="p-4 w-[130px] h-[130px] sm:w-[140px] sm:h-[140px] md:w-[150px] md:h-[150px] lg:w-[200px] lg:h-[200px] bg-blue-50 rounded-custom-br cursor-pointer"
+    onClick={onClick}
+  >
     <div className="overflow-hidden aspect-square flex items-center justify-center">
       <img src={src} alt={alt} className="w-full h-full object-cover" />
     </div>
@@ -14,6 +17,7 @@ const Metode = () => {
   const [methods, setMethods] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const scrollRefs = useRef([]);
 
   useEffect(() => {
     const loadMethods = async () => {
@@ -31,6 +35,15 @@ const Metode = () => {
     loadMethods();
   }, []);
 
+  const scrollToIndex = (index) => {
+    if (scrollRefs.current[index]) {
+      scrollRefs.current[index].scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   const renderFormattedDescription = (description) => {
     let formattedText = description.replace(/\n/g, "<span class='block mb-2'></span>");
 
@@ -45,7 +58,7 @@ const Metode = () => {
   };
 
   if (loading) {
-    return <LoadingIndicator/>;
+    return <LoadingIndicator />;
   }
 
   if (error) {
@@ -65,25 +78,32 @@ const Metode = () => {
             </div>
             <div className="flex justify-center md:justify-end">
               <div className="overflow-hidden md:block hidden">
-                <img src="https://res.cloudinary.com/dwaopoeya/image/upload/v1736190549/assets/aef6jwslgklxeijpapuy.png" alt="Profile" className="w-1/2 h-auto object-cover mx-auto" />
+                <img
+                  src="https://res.cloudinary.com/dwaopoeya/image/upload/v1736190549/assets/aef6jwslgklxeijpapuy.png"
+                  alt="Profile"
+                  className="w-1/2 h-auto object-cover mx-auto"
+                />
               </div>
             </div>
           </div>
         </div>
 
-        <div className="flex justify-center items-center mt-2 sm:mt-3 md:mt-4 lg:mt-6">
-          <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 place-content-center">
+        <div className="flex justify-center items-center mt-3 md:mt-4 lg:mt-6">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 place-content-center">
             {methods.map((method, index) => (
-              <ImageCard key={index} src={method.picture} alt={method.name} />
+              <ImageCard key={index} src={method.picture} alt={method.name} onClick={() => scrollToIndex(index)} />
             ))}
           </div>
         </div>
 
-        <div className="mt-20 text-custom-black mx-auto">
-          <div className="max-w-full mx-auto px-0 md:px-2 lg:px-10 xl:px-12 space-y-20">
+        <div className="mt-3 sm:mt-6 md:mt-10 lg:mt-16 xl:mt-20 text-custom-black mx-auto">
+          <div className="max-w-full mx-auto px-0 md:px-2 lg:px-10 xl:px-12 space-y-4 sm:space-y-8 md:space-y-11 lg:space-y-16 xl:space-y-20">
             {methods.map((method, index) => (
-              <div key={method.id} className="grid grid-cols-1 md:grid-cols-2 items-center gap-8 px-4 md:px-0">
-
+              <div
+                key={method.id}
+                ref={(el) => (scrollRefs.current[index] = el)}
+                className="grid grid-cols-1 md:grid-cols-2 items-center gap-8 px-4 md:px-0"
+              >
                 <div className={`title-content space-y-4 ${index % 2 === 0 ? "" : "md:order-2"}`}>
                   <h1 className="text-2xl lg:text-4xl xl:text-6xl font-extrabold tracking-wide">
                     {method.name.toUpperCase()}
@@ -95,7 +115,7 @@ const Metode = () => {
                 </div>
 
                 <div
-                  className={`flex justify-center bg-slate-50 shadow-lg w-full h-auto sm:h-[400px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] rounded-custom-br overflow-hidden ${
+                  className={`flex justify-center bg-slate-50 shadow-lg w-full h-[200px] sm:h-[400px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] rounded-custom-br overflow-hidden ${
                     index % 2 !== 0 ? "" : "justify-self-end"
                   }`}
                 >
