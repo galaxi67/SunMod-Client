5eb0974614ff75ea751c4c73c89a9fcf9fc023bd
ways92 <d.ferdiansya@gmail.com>
2025-01-18 02:11:32 +0700

    50% style homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -71,8 +71,14 @@ const Homepage = () => {
         <ServiceCard />
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="flex justify-center items-center md:order-2">
-            <span>BERBAGAI METODE YANG KAMI GUNAKAN</span>
+          <div className="flex justify-center items-start md:order-2 flex-col gap-1 xl:gap-3">
+            <p className="font-bold text-gray-400 text-base md:text-xl xl:text-2xl">Metode sumod</p>
+            <h1 className="font-semibold text-2xl md:text-3xl lg:text-5xl xl:text-6xl text-custom-black">Solusi tepat dengan metode handal</h1>
+            <div className="flex flex-wrap gap-1 md:gap-2 xl:gap-3 mt-1 md:mt-2 xl:mt-3">
+              <span className="border-2 border-sumod-bl3 rounded-full px-3 text-custom-black">metode</span>
+              <span className="border-2 border-custom-yellow rounded-full px-3 text-custom-black">sumod</span>
+              <span className="border-2 border-custom-orange rounded-full px-3 text-custom-black">sunat modern</span>
+            </div>
           </div>
 
           <div className="flex justify-center md:order-1">
@@ -90,9 +96,21 @@ const Homepage = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 mt-4">
-          <div className="flex justify-start items-center">LAYANAN SUMOD</div>
-          <div className="relative py-4 col-span-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-4">
+          <div className="flex justify-center items-start flex-col gap-1 xl:gap-3">
+            <p className="font-bold text-gray-400 text-base md:text-xl xl:text-2xl">Layanan sumod</p>
+            <h1 className="font-semibold text-2xl md:text-3xl lg:text-5xl xl:text-6xl text-custom-black">Solusi sunat yang menyeluruh</h1>
+            <div className="flex flex-wrap gap-1 md:gap-2 xl:gap-3 mt-1 md:mt-2 xl:mt-3">
+              <span className="border-2 border-sumod-bl3 rounded-full px-3 text-custom-black">layanan</span>
+              <span className="border-2 border-custom-yellow rounded-full px-3 text-custom-black">sunat modern</span>
+              <span className="border-2 border-sumod-bl rounded-full px-3 text-custom-black">nyaman</span>
+              <span className="border-2 border-custom-orange rounded-full px-3 text-custom-black">terpercaya</span>
+              <span className="border-2 border-sumod-bl4 rounded-full px-3 text-custom-black">professional</span>
+              <span className="border-2 border-sidebar rounded-full px-3 text-custom-black">terbaik</span>
+              <span className="border-2 border-custom-pink rounded-full px-3 text-custom-black">tidak sakit</span>
+            </div>
+          </div>
+          <div className="relative py-4">
             <div className="flex gap-6 overflow-x-auto">
               {Array.isArray(services) &&
                 services.map((service, index) => (
@@ -135,13 +153,22 @@ const Homepage = () => {
           </div>
         </div>
 
-        <div className="mt-5 px-4 sm:px-6 lg:px-8 max-w-screen-xl mx-auto">
-          <div className="flex justify-center mb-5">ARTIKEL</div>
-          <div className="relative flex gap-3 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-50">
+        <div className="mt-5 p-4 sm:p-6 lg:p-8 max-w-screen-xl mx-auto rounded-custom-br">
+          <div className="flex justify-center items-start md:items-center flex-col gap-1 xl:gap-3 mb-5">
+            <h1 className="font-semibold text-2xl lg:text-3xl xl:text-4xl text-custom-black">Artikel Kesehatan Sumod</h1>
+            <div className="flex flex-wrap gap-1 md:gap-2 xl:gap-3 mt-1 md:mt-2 xl:mt-3">
+              <span className="border-2 border-sumod-bl3 rounded-full px-3 text-custom-black">kesehatan</span>
+              <span className="border-2 border-custom-yellow rounded-full px-3 text-custom-black">berita</span>
+              <span className="border-2 border-custom-pink rounded-full px-3 text-custom-black">hidup sehat</span>
+              <span className="border-2 border-custom-orange rounded-full px-3 text-custom-black">artikel</span>
+              <span className="border-2 border-sumod-bl4 rounded-full px-3 text-custom-black">sunat modern</span>
+            </div>
+          </div>
+          <div className="relative flex gap-3 overflow-x-auto">
             {articles.map((article) => (
               <div
                 key={article.id}
-                className="bg-white shadow-custom rounded-custom-br overflow-hidden flex-none w-[120px] sm:w-[350px] lg:w-[280px]"
+                className="bg-white border shadow-custom rounded-custom-br overflow-hidden flex-none w-[120px] sm:w-[350px] md:w-[200px] lg:w-[250px]"
               >
                 <img
                   src={article.picture}
@@ -167,19 +194,6 @@ const Homepage = () => {
           </div>
         </div>
 
-        <div className="mt-5 w-full h-52 md:h-60 xl:h-96 rounded-md overflow-hidden">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1973.2786904792183!2d114.18130023967282!3d-8.445050299999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd3ffa211da2de5%3A0x8ed2a28fe6ab1af3!2sPraktek%20Dokter%20umum%20dr.%20Fills%20Prayoga%20B%20%2B%20Sunat%20Modern%20SUMOD!5e0!3m2!1sid!2sid!4v1733740630123!5m2!1sid!2sid"
-            width="100%"
-            height="100%"
-            className="border-0 rounded-md"
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            title="Google Maps"
-          ></iframe>
-        </div>
-
         <div className="grid grid-row-2 p-4 lg:p-6 mt-4">
           <div className="flex justify-center">
             <p className="text-slate-600 border-y-2 border-slate-300 text-center py-2 px-4 tracking-wider text-sm md:text-2xl lg:text-3xl xl:text-4xl font-extrabold mb-6">
@@ -199,6 +213,19 @@ const Homepage = () => {
               className="w-[120px] md:w-[250px] lg:w-[300px] xl:w-[400px] h-auto mx-auto"
             />
           </div>
+        </div>
+
+        <div className="mt-5 w-full h-52 md:h-60 xl:h-96 rounded-md overflow-hidden">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1973.2786904792183!2d114.18130023967282!3d-8.445050299999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd3ffa211da2de5%3A0x8ed2a28fe6ab1af3!2sPraktek%20Dokter%20umum%20dr.%20Fills%20Prayoga%20B%20%2B%20Sunat%20Modern%20SUMOD!5e0!3m2!1sid!2sid!4v1733740630123!5m2!1sid!2sid"
+            width="100%"
+            height="100%"
+            className="border-0 rounded-md"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Google Maps"
+          ></iframe>
         </div>
       </div>
     </div>
