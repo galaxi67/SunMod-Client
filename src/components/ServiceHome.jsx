1dcb200e348fa906d11b9031d2e5e81c33cfc889
ways92 <wayslos92@gmail.com>
2025-01-21 00:15:40 +0700

    penambahan swipper

--- src/components/ServiceHome.jsx
+++ src/components/ServiceHome.jsx
@@ -1,3 +1,6 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 import {
   UserIcon,
   ShieldCheckIcon,
@@ -7,7 +10,6 @@ import {
   ArrowRightIcon,
   ArrowLeftIcon,
 } from "@heroicons/react/24/solid";
-import { useRef } from "react";
 
 const services = [
   {
@@ -43,19 +45,17 @@ const services = [
 ];
 
 const ServiceCard = () => {
-  const scrollRef = useRef(null);
+  const [swiperInstance, setSwiperInstance] = useState(null);
+  const swiperRef = useRef(null);
 
-  const scrollLeft = () => {
-    scrollRef.current.scrollBy({ left: -200, behavior: "smooth" });
+  const handleSwiper = (swiper) => {
+    setSwiperInstance(swiper);
   };
 
-  const scrollRight = () => {
-    scrollRef.current.scrollBy({ left: 200, behavior: "smooth" });
-  };
   return (
     <div className="px-0 md:px-4 py-10">
       <div className="grid grid-cols-1 xl:grid-cols-2">
-        <div className="p-0 md:p-2 space-y-2 lg:space-y-4 w-3/4">
+        <div className="p-0 md:p-2 space-y-2 lg:space-y-4 xl:w-3/4">
           <h1 className="text-2xl font-bold text-sumod-bl3">Sunat Modern</h1>
           <p className="text-start text-base font-medium text-gray-600">
             Kami menawarkan berbagai layanan sunat yang disesuaikan dengan kebutuhan dan kenyamanan pasien dengan metode
@@ -66,38 +66,60 @@ const ServiceCard = () => {
               Lihat Layanan Kami
               <ArrowRightIcon className="w-5 h-5 ml-2 text-custom-orange animate-move-x" />
             </a>
-            <div className="relative flex gap-4 justify-end py-4 px-4 items-center">
-              <button
-                onClick={scrollLeft}
-                className="border border-sumod-bl3 p-2 rounded-full text-gray-800 hover:bg-sumod-bl3 hover:text-white hidden sm:flex"
-              >
-                <ArrowLeftIcon className="w-5 h-5" />
-              </button>
-              <button
-                onClick={scrollRight}
-                className=" border border-sumod-bl3 p-2 rounded-full text-gray-800 hover:bg-sumod-bl3 hover:text-white hidden sm:flex"
-              >
-                <ArrowRightIcon className="w-5 h-5" />
-              </button>
+            {swiperInstance && (
+              <div className="relative flex gap-4 justify-end py-4 px-4 items-center">
+                <button
+                  onClick={() => swiperInstance.slidePrev()} 
+                  className="border border-sumod-bl3 p-2 rounded-full text-gray-800 hover:bg-sumod-bl3 hover:text-white hidden sm:flex"
+                >
+                  <ArrowLeftIcon className="w-5 h-5" />
+                </button>
+                <button
+                  onClick={() => swiperInstance.slideNext()} 
+                  className=" border border-sumod-bl3 p-2 rounded-full text-gray-800 hover:bg-sumod-bl3 hover:text-white hidden sm:flex"
+                  >
+                  <ArrowRightIcon className="w-5 h-5" />
+                </button>
+              </div>
+              )}
             </div>
           </div>
-        </div>
 
-        <div ref={scrollRef} className="flex gap-6 overflow-x-auto no-scrollbar">
-          {services.map((service) => (
-            <div
-              key={service.id}
-              className="flex-shrink-0 mx-auto rounded-lg p-6 flex flex-col text-start bg-sumod-bl2 text-gray-800"
-            >
-              <div className="w-48 sm:w-56 md:w-60 lg:w-64 xl:w-72">
-                <div className="flex items-center gap-4">
-                  <div className=" rounded-full">{service.icon}</div>
-                  <h3 className="text-lg sm:text-xl font-semibold text-gray-800">{service.title}</h3>
+        <div className="relative flex gap-6 overflow-x-auto no-scrollbar">
+          <Swiper
+            ref={swiperRef}
+            onSwiper={handleSwiper}
+            spaceBetween={10}
+            slidesPerView={1}
+            loop={true}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+            }}
+            className="py-6"
+          >
+            {services.map((service) => (
+              <SwiperSlide key={service.id}>
+                <div className="flex-shrink-0 mx-auto rounded-lg p-6 flex flex-col text-start bg-sumod-bl2 text-gray-800">
+                  <div className="w-full h-32 xl:h-48">
+                    <div className="flex items-center gap-4">
+                      <div className="rounded-full">{service.icon}</div>
+                      <h3 className="text-lg sm:text-xl font-semibold text-gray-800">{service.title}</h3>
+                    </div>
+                    <p className="text-sm sm:text-base text-gray-600 mt-2">{service.description}</p>
+                  </div>
                 </div>
-                <p className="text-sm sm:text-base text-gray-600 mt-2">{service.description}</p>
-              </div>
-            </div>
-          ))}
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </div>
